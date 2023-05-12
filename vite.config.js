c6daf22a5a7d3f3c03957e64a41ca0f67cd24468
小楓 <cathy_wu@mail.chungyo.net>
2023-05-12 11:44:22 +0800

    修正環境設定路徑

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: './',
+  // 影響 running build 產生 index.html 檔案之路徑
+  // https://stackoverflow.com/questions/69744253/vite-build-always-using-static-paths
+  base: './',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
