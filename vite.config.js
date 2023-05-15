623d767ab565c936727906fd8b67eb4b8f1081bd
小楓 <cathy_wu@mail.chungyo.net>
2023-05-15 11:16:56 +0800

    測試環境設定路徑

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
@@ -8,7 +8,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 export default defineConfig({
   // 影響 running build 產生 index.html 檔案之路徑
   // https://stackoverflow.com/questions/69744253/vite-build-always-using-static-paths
-  base: '/dist/',
+  base: '/vueInfiniteScroll/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
@@ -20,7 +20,6 @@ export default defineConfig({
       '/api': {
         target: 'https://api.github.com/orgs/nodejs/repos',
         changeOrigin: true,
-        // secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
